60dd75e00e89a24be7771a9e66fc04268e3b2c80
stormymayday <stormymayday7@gmail.com>
2024-11-26 20:01:43 -0800

    values.js library

--- components/ColorGenerator/index.tsx
+++ components/ColorGenerator/index.tsx
@@ -1,7 +1,15 @@
+"use client";
+
 import Form from "./Form";
 import ColorList from "./ColorList";
+import Values from "values.js";
+import { useState } from "react";
 
 function ColorGenerator() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [colors, setColors] = useState([new Values("#f15025").all(10)]);
+    console.log(colors);
+
     return (
         <main>
             <Form />
